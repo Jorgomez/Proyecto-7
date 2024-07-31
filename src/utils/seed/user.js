72b97c88f12118740a9950afc2d205495c7b96a2
Jorge <jorgomez@protonmail.com>
2024-07-31 14:20:12 +0100

    First Touch

--- src/utils/seed/user.js
+++ src/utils/seed/user.js
@@ -1,24 +1,24 @@
-const User = require('../../api/models/user')
-const mongoose = require('mongoose')
-const USERS = require('../../data/users')
+// const User = require('../../api/models/user')
+// const mongoose = require('mongoose')
+// const USERS = require('../../data/users')
 
-const bombUsers = async () => {
-  try {
-    await mongoose.connect(
-      'mongodb+srv://jorgomez:root@cluster0.5mxtwul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    )
+// const bombUsers = async () => {
+//   try {
+//     await mongoose.connect(
+//       'mongodb+srv://jorgomez:root@cluster0.5mxtwul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+//     )
 
-    await User.collection.drop()
-    console.log('Users deleted')
+//     await User.collection.drop()
+//     console.log('Users deleted')
 
-    await User.insertMany(USERS)
-    console.log('Users introduced')
+//     await User.insertMany(USERS)
+//     console.log('Users introduced')
 
-    await mongoose.disconnect()
-    console.log('server desconected')
-  } catch (error) {
-    console.log('error connecting')
-  }
-}
+//     await mongoose.disconnect()
+//     console.log('server desconected')
+//   } catch (error) {
+//     console.log('error connecting')
+//   }
+// }
 
-bombUsers()
+// bombUsers()
